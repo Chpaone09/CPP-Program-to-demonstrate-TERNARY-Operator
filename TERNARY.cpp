a9a405a6b0074d47dc4144df8383f1ebbf468ed1
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-19 02:45:11 +0530

    Program to demonstrate TERNARY Operator in cpp

---
+++ TERNARY.cpp
@@ -0,0 +1,22 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate TERNARY Operator in cpp.
+Author  : Chpaone09®
+Date    : Oct 19, 2020
+
+************************************************************
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 2, b = 5;
+    
+    cout<<"\n\n\t Bigger Number is >> "<< (a > b ? a : b);
+
+    return 0;
+    
+}
